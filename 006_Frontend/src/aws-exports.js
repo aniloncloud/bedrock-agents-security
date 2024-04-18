420c52d52d000e62ef6e63c111ee8dfe6d5c0cc0
EC2 Default User <ec2-user@ip-172-31-2-161.ec2.internal>
2024-04-18 01:08:58 +0000

    Frontend

---
+++ 006_Frontend/src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_281NgjhEf',
+  aws_user_pools_web_client_id: '71q2tf4cbvlfekkrnhh0l613nc',
+};
+
+export default awsConfig;
