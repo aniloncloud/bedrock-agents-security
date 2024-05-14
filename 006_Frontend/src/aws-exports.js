248b894a43c245cba2474d541d9a399879208c03
EC2 Default User <anilnadi@amazon.com>
2024-05-14 00:07:52 +0000

    Ignore Amplify directory and files

--- 006_Frontend/src/aws-exports.js
+++
@@ -1,9 +0,0 @@
-const awsConfig = {
-  aws_project_region: "us-east-1" ,
-  aws_cognito_region: "us-east-1" ,
-  aws_user_pools_id: "us-east-1_3gC3Yubbn" ,
-  aws_user_pools_web_client_id: "37aits10oup0mgnbb0m99ia5fp",
-
-};
-
-export default awsConfig;
